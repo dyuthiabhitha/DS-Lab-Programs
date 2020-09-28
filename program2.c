387773db3f3476f91c9a1da2a032ef9a7e7baa00
dyuthiabhitha <71492406+dyuthiabhitha@users.noreply.github.com>
2020-09-28 06:11:19 +0100

    Create program2.c

---
+++ program2.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define stack_size 5
+int top=-1;
+int s[10];
+int item;
+void push()
+{
+    if(top==stack_size-1)
+    {
+        printf("\nStack Overflow\n");
+        return;
+    }
+    top+=1;
+    s[top]=item;
+}
+int pop()
+{
+    if(top==-1)
+        return -1;
+    return s[top--];    
+}
+void display()
+{
+    int i;
+    if(top==-1)
+    {
+        printf("\nStack is empty\n");
+        return;
+    }
+    printf("The contents of the stack are:\n");
+    for (i=0;i<=top;i++)
+    {
+        printf("%d\n",s[i]);
+    }
+}
+void main()
+{
+    int item_deleted,choice;
+    while(1)
+    {
+        printf("\n1:Push\n2:Pop\n3:Display\n4:Exit\n");
+        printf("Enter your choice:");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1: 
+            printf("\nEnter the item to be inserted:");
+            scanf("%d",&item);
+            push();
+            break;
+            case 2:
+            item_deleted=pop();
+            if(item_deleted==-1)
+            {
+                printf("\nStack is empty\n");
+            } 
+            else
+            {
+                printf("\nThe item deleted is %d\n",item_deleted);
+            }
+            break;
+            case 3:
+            display();
+            break;
+            default:exit(0);
+
+
+        }
+    }
+
+}
